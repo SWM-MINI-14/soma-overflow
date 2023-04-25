bf6297bb61b84646f4b0da03a52f19e27b5b26d6
qoquma <euichan1108@gmail.com>
2023-04-26 01:58:14 +0900

    Random Avartar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { AppBar, Toolbar, Typography, Button } from '@mui/material';
 import { makeStyles } from '@mui/styles';
 import { Link } from 'react-router-dom';
+import Logo from '../assets/smoverflow_horizontal.png';
 
 const useStyles = makeStyles({
     title: {
@@ -21,7 +22,9 @@ const NavBar = () => {
         <AppBar position="static">
             <Toolbar>
                 <Typography variant="h6" className={classes.title}>
-                    <Link to="/" className={classes.link}>StackOverflow Clone</Link>
+                    <Link to="/" className={classes.link}>
+                        <img alt={'logo'} src={Logo} style={{maxHeight: '50px', maxWidth: '150px'}} />
+                    </Link>
                 </Typography>
                 <Link to="/login" className={classes.link}>
                     <Button color="inherit">로그인</Button>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Container, Typography, List, ListItem, ListItemText, ListItemAvatar, Avatar } from '@mui/material';
 import { makeStyles } from '@mui/styles';
+import { identiconSvg } from 'https://cdn.jsdelivr.net/npm/minidenticons@3.1.2/minidenticons.min.js'
 
 const useStyles = makeStyles({
     container: {
@@ -39,7 +40,7 @@ const Home = () => {
                 {questions.map(question => (
                     <ListItem key={question.question_id} className={classes.listItem} component="a" href={`/questions/${question.question_id}`} alignItems="flex-start">
                         <ListItemAvatar>
-                            <Avatar src={question.owner.profile_image} />
+                            <identicon-svg username={question.owner.display_name}></identicon-svg>
                         </ListItemAvatar>
                         <ListItemText primary={question.title} secondary={`작성자: ${question.owner.display_name}`} />
                     </ListItem>
